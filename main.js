dd3600ec0f9b2a0f7a7a2d683b74da2fbf31f6b4
odrac94 <134649565+odrac94@users.noreply.github.com>
2023-09-22 18:15:37 -0700

    Update

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-// import { Client } from '@notionhq/client'
-
-// const DATABASE_ID = '39699e926a6e47d8a6617485d8b46e38'
-
-// const notion = new Client({
-//     auth: import.meta.env.NOTION_TOKEN
-// })
-
-// NOTION_TOKEN="secret_doUhA3ATP574RVDlI6KuwOrmlIApKFR0t4zz6H5RA2B"
-
 // ARRAY DE STATS
 const personajeList = [
 [
